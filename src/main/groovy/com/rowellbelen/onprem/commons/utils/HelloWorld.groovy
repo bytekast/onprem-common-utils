af7f8d9d9eead8966f1f766f01657543c60dcb6a
Rowell Belen <rowell.belen@bytekast.com>
2015-11-09 23:17:26 -0500

    project bootstrap

---
+++ src/main/groovy/com/rowellbelen/onprem/commons/utils/HelloWorld.groovy
@@ -0,0 +1,12 @@
+package com.rowellbelen.onprem.commons.utils
+
+class HelloWorld {
+
+  String getMessage(boolean bigger) {
+    if (bigger) {
+      return "Hello Universe!";
+    } else {
+      return "Hello World!";
+    }
+  }
+}
